1c2dd7fe6ed536549efdb4b2faadcf95fa5ed6f3
cupofjoy <eelantung@gmail.com>
2018-11-09 19:02:36 -0500

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,77 @@
 // authentication, username, password, etc
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+
+// Load User model
+const User = require('../../models/User');
 
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "Users Works"}));
+
+// @route   GET api/users/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if(user) {
+        return res.status(400).json({email: "Email already exists"})
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', //size
+          r: 'pg', //rating
+          d: 'mm' //default
+        });
+
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err))
+          });
+        });
+      }
+  })
+});
+
+// @route   GET api/users/login
+// @desc    Login user / Returning JWT token
+// @access  Public
+router.post('/login', () => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by Email
+  User.findOne({email})
+    .then(user = {
+      // Check for user
+      if(!user) {
+        return res.status(404).json({email: 'User not found'});
+      }
+
+      // Check password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch) {
+            res.json({msg: 'Success'})
+          } else {
+            return res.status(400).json({password: 'Password incorrect'});
+          }
+        })
+    });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
 
 // DB Config
 const db = require('./config/keys').mongoURI;
