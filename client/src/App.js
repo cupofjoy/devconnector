e96b5fe218c36e1b3f1dcff635bdae2cabaab46b
cupofjoy <eelantung@gmail.com>
2018-11-21 22:48:13 -0500

    setting up state for Login and Register forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>My react App</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path='/' component={ Landing } />
+          <div className="container">
+            <Route exact path='/register' component={ Register } />
+            <Route exact path='/login' component={ Login } />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
