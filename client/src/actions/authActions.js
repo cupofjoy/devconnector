7f712e5df5f41c9b94c41875936acc11cb9fab3d
cupofjoy <eelantung@gmail.com>
2018-11-26 18:31:36 -0500

    setting up token once user logs in

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -13,3 +16,34 @@ export const registerUser = (userData, history) => dispatch => {
       })
     );
 };
+
+// Login - Get User Token
+export const loginUser = (userData) => dispatch => {
+  axios.post('/api/users/login', userData)
+    .then(res => {
+      // Save to localStorage
+      const { token } = res.data;
+      // Set token to localStorage
+      localStorage.setItem('jwtToken', token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    )
+};
+
+// Set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,7 @@
+import isEmpty from '../validation/is-empty';
+
+import { SET_CURRENT_USER } from '../actions/types';
+
 const initialState = {
   isAuthenticated: false,
   user: {}
@@ -5,6 +9,12 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      }
     default:
       return state;
   }
