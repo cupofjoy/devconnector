eccb4ba0b7c41007caaa6f0158fe0f8a6b0b285a
cupofjoy <eelantung@gmail.com>
2018-11-26 17:43:28 -0500

    finishing actions and reducers for registering users

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,9 +1,15 @@
-import { TEST_DISPATCH } from './types';
+import axios from 'axios';
+import { GET_ERRORS } from './types';
 
 // Register User
-export const registerUser = userData => {
-  return {
-    type: TEST_DISPATCH,
-    payload: userData
-  }
-}
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post('/api/users/register', userData)
+    .then(res => history.push('/login'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import axios from 'axios';
+import { withRouter } from 'react-router-dom';
 import classnames from 'classnames';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 import { registerUser } from '../../actions/authActions';
 
 class Register extends Component {
@@ -18,8 +18,14 @@ class Register extends Component {
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.errors) {
+      this.setState({ errors: nextProps.errors })
+    }
+  }
+
   onChange = (e) => {
-    this.setState({[e.target.name]: e.target.value})
+    this.setState({ [e.target.name]: e.target.value })
   }
 
   onSubmit = (e) => {
@@ -32,22 +38,14 @@ class Register extends Component {
       password2: this.state.password2
     }
 
-    this.props.registerUser(newUser);
-
-    // axios
-    //   .post('/api/users/register', newUser)
-    //   .then(res => console.log(res.data))
-    //   .catch(err => this.setState({errors: err.response.data }))
+    this.props.registerUser(newUser, this.props.history);
   }
 
   render() {
     const { errors } = this.state;
 
-    const { user } = this.props.auth;
-
     return (
       <div className="register">
-        {user ? user.name : null}
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -127,11 +125,13 @@ class Register extends Component {
 
 Register.propTypes = {
   registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = (state) => ({
-  auth: state.auth
+  auth: state.auth,
+  errors: state.errors
 })
 
-export default connect(mapStateToProps, { registerUser })(Register)
+export default connect(mapStateToProps, { registerUser })(withRouter(Register))
