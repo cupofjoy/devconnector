9db013ee40f8f4485fc3c5b67bd834791e547570
cupofjoy <eelantung@gmail.com>
2018-12-12 18:00:30 -0500

    starting Experience and Education portion of profile

---
+++ client/src/components/add-credentials/AddExperience.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { TextFieldGroup } from '../common/TextFieldGroup';
+import { TextAreaFieldGroup } from '../common/TextAreaFieldGroup';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class AddExperience extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      company: '',
+      title: '',
+      location: '',
+      from: '',
+      to: '',
+      current: false,
+      description: '',
+      errors: {},
+      disabled: false
+    }
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+AddExperience.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  errors: state.errors
+})
+
+export default connect(mapStateToProps)(withRouter(AddExperience));
