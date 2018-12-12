db0f414fbba8f83263689f01a2b5253ebbb24017
cupofjoy <eelantung@gmail.com>
2018-12-12 00:52:41 -0500

    adding form and setting state for edit profile

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
+import CreateProfile from './components/edit-profile/EditProfile';
 
 import './App.css';
 
@@ -57,6 +58,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/edit-profile" component={EditProfile} />
               </Switch>
             </div>
             <Footer />
