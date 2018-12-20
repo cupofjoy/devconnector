a26a76f29422df61c07e4d21a2186c4ca9726b86
cupofjoy <eelantung@gmail.com>
2018-12-20 02:27:19 -0500

    adding link to list of created profiles

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Spinner from '../common/Spinner';
+import { getProfiles } from '../../actions/profileActions';
+
+class Profiles extends Component {
+  componentDidMount() {
+    this.props.getProfiles();
+  }
+
+  render() {
+    const { profiles, loading } = this.props.profile;
+    let profileItems;
+
+    if(profiles === null || loading) {
+      profileItems = <Spinner />
+    } else {
+      if(profiles.length > 0) {
+        <h1>PROFILES HERE</h1>
+      } else {
+        ProfileItems = <h4>No Profiles found...</h4>
+      }
+    }
+
+    return (
+      <div className="profiles">
+        <div className="conatiner">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Developer Profiles</h1>
+              <p className="lead text-center">
+                Browse and connect with developers
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) =>  ({
+  profile: state.profile
+})
+
+export default connect(mapStateToProps, { getProfiles })(Profiles);
