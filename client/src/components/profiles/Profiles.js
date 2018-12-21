ad569e4bb9c0c17f4e36ea375504e25bda0933f9
cupofjoy <eelantung@gmail.com>
2018-12-21 01:59:11 -0500

    finished listing all developer profiles

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Spinner from '../common/Spinner';
 import { getProfiles } from '../../actions/profileActions';
+import ProfileItem from './ProfileItem'
 
 class Profiles extends Component {
   componentDidMount() {
@@ -17,7 +18,9 @@ class Profiles extends Component {
       profileItems = <Spinner />
     } else {
       if (profiles.length > 0) {
-        profileItems = <h1>PROFILES HERE</h1>;
+        profileItems = profiles.map(profile => (
+          <ProfileItem key={profile._id} profile={profile} />
+        ))
       } else {
         profileItems = <h4>No Profiles found...</h4>
       }
