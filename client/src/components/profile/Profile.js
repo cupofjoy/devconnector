253b92b9c533c10e301f0fdc0573a60ec7f223b4
cupofjoy <eelantung@gmail.com>
2018-12-23 22:40:46 -0500

    finished profile header

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -18,9 +18,39 @@ class Profile extends Component {
   }
 
   render() {
-    return (
-      <div>
+    const { profile, loading } = this.props.profile;
+    let profileContent;
 
+    if(profile === null || loading) {
+      profileContent = <Spinner />
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/profiles" className="btn btn-light mb-3 float-left">
+                Back To Profiles
+              </Link>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <ProfileHeader profile={profile} />
+          <ProfileAbout />
+          <ProfileCreds />
+          <ProfileGithub />
+        </div>
+      )
+    }
+
+    return (
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              {profileContent}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
